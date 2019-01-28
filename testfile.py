710de28b2f0e3cef501087223aa38550795f5296
Isha <irai@ucvts.org>
2019-01-28 13:45:08 -0500

    made sprites folder

--- testfile.py
+++ testfile.py
@@ -1,24 +1,23 @@
+# Import all needed extensions
 import os
 import sys
 import pygame
 import random
 from pygame import *
 pygame.init()
-
+# define constants on screem (color, size, score, time)
 scr_size = (width,height) = (600,150)
 FPS = 60
 gravity = 0.6
-
 black = (0,0,0)
 white = (255,255,255)
 background_col = (235,235,235)
-
 high_score = 0
-
 screen = pygame.display.set_mode(scr_size)
 clock = pygame.time.Clock()
 
-
+# create function to bring image into file
+# make image compatible with code
 def load_image(
     name,
     sizex=-1,
@@ -38,21 +37,139 @@ def load_image(
         image = pygame.transform.scale(image, (sizex, sizey))
 
     return (image, image.get_rect())
+    
+# load 
+def load_sprite_sheet(
+        sheetname,
+        nx,
+        ny,
+        scalex = -1,
+        scaley = -1,
+        colorkey = None,
+        ):
+    fullname = os.path.join('sprites',sheetname)
+    sheet = pygame.image.load(fullname)
+    sheet = sheet.convert()
 
-class Player:
-# creates player and contains information on qualities
-    def __init__(self, name, speed, filename):
-        self.name = name
-        self.speed = speed
-        self.filename = filename
-loadghost = pygame.image.load("ghost.png")
-ghostfile = pygame.Surface.convert_alpha(loadghost)
-loaddino = pygame.image.load("dinosaur.png")
-dinofile = pygame.Surface.convert_alpha(loaddino)
-ghost = Player('ghost', 2, ghostfile)
-dino = Player('dino', 2, dinofile)
-ghostrect = ghostfile.get_rect()
-dinorect = dinofile.get_rect()
+    sheet_rect = sheet.get_rect()
+
+    sprites = []
+
+    sizex = sheet_rect.width/nx
+    sizey = sheet_rect.height/ny
+
+    for i in range(0,ny):
+        for j in range(0,nx):
+            rect = pygame.Rect((j*sizex,i*sizey,sizex,sizey))
+            image = pygame.Surface(rect.size)
+            image = image.convert()
+            image.blit(sheet,(0,0),rect)
+
+            if colorkey is not None:
+                if colorkey is -1:
+                    colorkey = image.get_at((0,0))
+                image.set_colorkey(colorkey,RLEACCEL)
+
+            if scalex != -1 or scaley != -1:
+                image = pygame.transform.scale(image,(scalex,scaley))
+
+            sprites.append(image)
+
+    sprite_rect = sprites[0].get_rect()
+
+    return sprites,sprite_rect
+
+def disp_gameOver_msg(retbutton_image,gameover_image):
+    retbutton_rect = retbutton_image.get_rect()
+    retbutton_rect.centerx = width / 2
+    retbutton_rect.top = height*0.52
+
+    gameover_rect = gameover_image.get_rect()
+    gameover_rect.centerx = width / 2
+    gameover_rect.centery = height*0.35
+
+    screen.blit(retbutton_image, retbutton_rect)
+    screen.blit(gameover_image, gameover_rect)
+
+def extractDigits(number):
+    if number > -1:
+        digits = []
+        i = 0
+        while(number/10 != 0):
+            digits.append(number%10)
+            number = int(number/10)
+
+        digits.append(number%10)
+        for i in range(len(digits),5):
+            digits.append(0)
+        digits.reverse()
+        return digits
+
+class Dino():
+    def __init__(self,sizex=-1,sizey=-1):
+        self.images,self.rect = load_sprite_sheet('dino.png',5,1,sizex,sizey,-1)
+        self.images1,self.rect1 = load_sprite_sheet('dino_ducking.png',2,1,59,sizey,-1)
+        self.rect.bottom = int(0.98*height)
+        self.rect.left = width/15
+        self.image = self.images[0]
+        self.index = 0
+        self.counter = 0
+        self.score = 0
+        self.isJumping = False
+        self.isDead = False
+        self.isDucking = False
+        self.isBlinking = False
+        self.movement = [0,0]
+        self.jumpSpeed = 11.5
+        self.stand_pos_width = self.rect.width
+        self.duck_pos_width = self.rect1.width
+
+    def draw(self):
+        screen.blit(self.image,self.rect)
+
+    def checkbounds(self):
+        if self.rect.bottom > int(0.98*height):
+            self.rect.bottom = int(0.98*height)
+            self.isJumping = False
+
+    def update(self):
+        if self.isJumping:
+            self.movement[1] = self.movement[1] + gravity
+
+        if self.isJumping:
+            self.index = 0
+        elif self.isBlinking:
+            if self.index == 0:
+                if self.counter % 400 == 399:
+                    self.index = (self.index + 1)%2
+            else:
+                if self.counter % 20 == 19:
+                    self.index = (self.index + 1)%2
+
+        elif self.isDucking:
+            if self.counter % 5 == 0:
+                self.index = (self.index + 1)%2
+        else:
+            if self.counter % 5 == 0:
+                self.index = (self.index + 1)%2 + 2
+
+        if self.isDead:
+           self.index = 4
+
+        if not self.isDucking:
+            self.image = self.images[self.index]
+            self.rect.width = self.stand_pos_width
+        else:
+            self.image = self.images1[(self.index)%2]
+            self.rect.width = self.duck_pos_width
+
+        self.rect = self.rect.move(self.movement)
+        self.checkbounds()
+
+
+        self.counter = (self.counter + 1)
+
+
 
 x = 0
 y = 0
@@ -61,26 +178,3 @@ vely = 0
 accy = .1
 jumping = False
 
-
-
-while True:
-    for e in pygame.event.get():
-        if e.type == pygame.QUIT:
-            sys.exit()
-        if e.type == pygame.KEYDOWN:
-            jumping = True
-
-
-
-    for event in pygame.key.get_pressed(K_SPACE):
-        if event.type == pygame.QUIT: sys.exit()
-
-    ghostrect = ghostrect.move(speed)
-    if ghostrect.left < 0 or ghostrect.right > width:
-        speed[0] = -speed[0]
-    if ghostrect.top < 0 or ghostrect.bottom > height:
-        speed[1] = -speed[1]
-
-    screen.fill(white)
-    screen.blit(ghost, ghostrect)
-    pygame.display.flip()
